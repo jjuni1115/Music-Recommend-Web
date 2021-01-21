3e1a2fc68bada310ba3708b8d114bea172a3d770
kkj5158 <jjk5158@naver.com>
2021-01-21 11:11:49 +0900

    -로그온 페이지 로그인 폼 디자인 -기초적인 사이트 디자인 - 배경이미지 안올라감 ( 해결방법 찾는중 )

--- possible/src/main/java/com/MrS/possible/possibleController.java
+++ possible/src/main/java/com/MrS/possible/possibleController.java
@@ -10,6 +10,7 @@ public class possibleController {
     public String index(){
         return "index";
     }
+
 }
 
 
@@ -19,66 +20,65 @@ public class possibleController {
 
 
 
+/*
 
+    @ResponseBody
+    @PostMapping(value = "/login")
+    public String login(member m, HttpSession session){
+        return "login";
+        return m_dao.try_login(m, session);
+        }
 
+    @RequestMapping(value="TestPage.do", method=RequestMethod.POST)
+    public String TestPage(Model model, HttpServletRequest request){
+        String testInput = (String)request.getParameter(m);
 
-//    @ResponseBody
-//    @PostMapping(value = "/login")
-//    public String login(member m, HttpSession session){
-////        return "login";
-//        return m_dao.try_login(m, session);
-//    }
+        if (testInput.length() > 0){
+            model.addAttribute("Output", "Success");
+        }
+        else{
+           model.addAttribute("Output", "Failed");
+        }
 
-//    @RequestMapping(value="TestPage.do", method=RequestMethod.POST)
-//    public String TestPage(Model model, HttpServletRequest request){
-//        String testInput = (String)request.getParameter(m);
-//
-//        if (testInput.length() > 0){
-//            model.addAttribute("Output", "Success");
-//        }
-//        else{
-//            model.addAttribute("Output", "Failed");
-//        }
-//
-//        return "login";
-//    }
+        return "login";
+    }
 
-//    @RequestMapping(value = "/register", method = RequestMethod.GET)
-//    public String register() {
-//        return "register";
-//    }
-//
-//    @ResponseBody
-//    @RequestMapping(value = "/register", method = RequestMethod.POST)
-//    public int register(member m) {
-//        return m_dao.Register(m);
-//    }
-//
-//    @RequestMapping(value = "/index", method = RequestMethod.GET)
-//    public String index() {
-//        return "index";
-//    }
-//
-//    @RequestMapping(value = "/logout", method = RequestMethod.GET)
-//    public String logout(HttpSession session) {
-//        session.removeAttribute("m");
-//        return "redirect:/";
-//    }
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String register() {
+        return "register";
+    }
 
-//    @ControllerAdvice
-//    public class JsonpAdviceController extends AbstractJsonpResponseBodyAdvice {
-//        public JsonpAdviceController() {
-//            super("callback");
-//        }
-//    }
+    @ResponseBody
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public int register(member m) {
+        return m_dao.Register(m);
+    }
+/*
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+*/
+    //@RequestMapping(value = "/logout", method = RequestMethod.GET)
+   // public String logout(HttpSession session) {
+        //session.removeAttribute("m");
+       // return "redirect:/"; }
+/*
+    @ControllerAdvice
+    public class JsonpAdviceController extends AbstractJsonpResponseBodyAdvice {
+        public JsonpAdviceController() {
+            super("callback");
+        }
+    }
 
-//    @Bean
-//    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-//        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-//        jsonConverter.setObjectMapper(objectMapper);
-//        return jsonConverter;
-//    }
-
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+       ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        jsonConverter.setObjectMapper(objectMapper);
+        return jsonConverter;
+    }
+*/
 //}
+
